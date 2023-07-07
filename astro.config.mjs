4fbd9d5ba83c064ff342dbe78980cbdb768be652
Leo0114 <leonel0614@gmail.com>
2023-07-07 01:59:39 -0600

    ft:bodas

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://Leo0114.github.io",
   base: "/volcancafe",
-  integrations: [tailwind(), image(),],
+  integrations: [tailwind(), image(), react()]
 });
