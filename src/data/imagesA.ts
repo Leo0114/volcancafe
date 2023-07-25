93566550e5939561907aee5a31f334cecae7ad37
Leo0114 <leonel0614@gmail.com>
2023-07-25 02:36:11 -0600

    ft:lotOfimages

--- src/data/imagesA.ts
+++ src/data/imagesA.ts
@@ -1,3 +1,35 @@
+/*Bodas  */
+import boda1 from "@assets/boda/boda1.webp";
+import boda2 from "@assets/boda/dos.webp";
+import boda3 from "@assets/boda/tres.webp";
+import boda4 from "@assets/boda/cuatro.webp";
+import boda5 from "@assets/boda/boda2.webp";
+import boda6 from "@assets/boda/seis.webp";
+import boda7 from "@assets/boda/siete.webp";
+import boda8 from "@assets/boda/ocho.webp";
+import boda9 from "@assets/boda/nine.webp";
+import boda10 from "@assets/boda/ten.webp";
+import boda11 from "@assets/boda/once.webp";
+import boda12 from "@assets/boda/boda3.webp";
+import boda13 from "@assets/boda/trece.webp";
+import boda14 from "@assets/boda/catorce.webp";
+import boda15 from "@assets/boda/quince.webp";
+import boda16 from "@assets/boda/diesyseis.webp";
+import boda17 from "@assets/boda/doce.webp";
+import boda18 from "@assets/boda/uno.webp";
+import boda19 from "@assets/boda/cinco.webp";
+import mcEvento from "@assets/boda/mcEventos.webp";
+import rafael from "@assets/boda/sanRafael.webp";
+import shambala from "@assets/boda/shambala.webp";
+import snackBar from "@assets/boda/snacksBar.webp";
+import snackClub from "@assets/boda/snacksClub.webp";
+import teo from "@assets/boda/teo.webp";
+
+/* Contacto */
+import sectur from "@assets/contacto/sectur.webp";
+import canco from "@assets/contacto/canaco.webp";
+
+/* Layout */
 import aguaBoda from "@assets/layout/aguaBoda.webp";
 import fogatero from "@assets/layout/Fogatero.webp";
 import banner from "@assets/layout/banner.webp";
@@ -5,8 +37,44 @@ import camastros from "@assets/layout/camastros.webp";
 import intro from "@assets/layout/intro.webp";
 import meseros from "@assets/layout/meserosBoda.webp";
 import nocheCabaña from "@assets/layout/nocheCabaña.webp";
+/* Tours */
+import hongo from "@assets/tour/hongo.webp";
+import heli from "@assets/tour/helicopter.webp";
+import sayula from "@assets/tour/sayulita.webp";
+import huasteca from "@assets/tour/huasteca.webp";
+import isla from "@assets/tour/islaCocinas.webp";
+import guardias from "@assets/tour/losGuardias.webp";
 
 const images = {
+  boda1: boda1,
+  boda2: boda2,
+  boda3: boda3,
+  boda4: boda4,
+  boda5: boda5,
+  boda6: boda6,
+  boda7: boda7,
+  boda8: boda8,
+  boda9: boda9,
+  boda10: boda10,
+  boda11: boda11,
+  boda12: boda12,
+  boda13: boda13,
+  boda14: boda14,
+  boda15: boda15,
+  boda16: boda16,
+  boda17: boda17,
+  boda18: boda18,
+  boda19: boda19,
+  teo: teo,
+  snackBar: snackBar,
+  snackClub: snackClub,
+  rafael: rafael,
+  shambala: shambala,
+  mcEvento: mcEvento,
+
+  sectur: sectur,
+  canco: canco,
+
   aguaBoda: aguaBoda,
   fogatero: fogatero,
   banner: banner,
@@ -14,5 +82,12 @@ const images = {
   intro: intro,
   meseros: meseros,
   nocheCabaña: nocheCabaña,
+
+  heli: heli,
+  hongo: hongo,
+  sayula: sayula,
+  huasteca: huasteca,
+  isla: isla,
+  guardias: guardias,
 };
 export default images;
