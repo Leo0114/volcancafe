ee237ef098c8bf14bd16c706a65614e0e8f863bc
Leo0114 <leonel0614@gmail.com>
2023-08-03 16:13:58 -0600

    try

--- src/data/villas.ts
+++ src/data/villas.ts
@@ -21,7 +21,10 @@ export const villas = [
     fechasE: "Navidad y Año nuevo",
     fechasEminimoNoches: 2,
     fechasEPrecio: "3850",
-    texto: "",
+    descripcion:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut accusamus, adipisci quisquam quis impedit et magnam accusantium reiciendis explicabo, quod nemo, laborum aspernatur? Eos atque dolores dolor? Asperiores, alias corporis maiores ullam quia quis iure sit. Sed beatae officiis mollitia, est quia libero exercitationem eos doloribus quod atque labore odio voluptatum unde consequuntur in! Dolores, ipsam ratione officiis ducimus architecto alias doloribus quam iste ut quia at.",
+    where:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1881.6287084206322!2d-103.70081010702229!3d19.40128070949323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x84255d1d3cb8c1c7%3A0x93a4f240db0d7169!2sVolc%C3%A1n%20%26%20Caf%C3%A9%20-%20Villas%20-%20Hospedaje!5e0!3m2!1ses!2smx!4v1691095635984!5m2!1ses!2smx",
     promocion: "Preguntar por precios al ser menos de 4 personas",
     amenidades: ["Cocina equipada", "TV", "Wifi", "Estacionamiento"],
     reglas: [""],
@@ -43,6 +46,10 @@ export const villas = [
     fechasE: "Navidad y Año Nuevo",
     fechasEminimoNoches: 2,
     fechasEPrecio: "4,200",
+    descripcion:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut accusamus, adipisci quisquam quis impedit et magnam accusantium reiciendis explicabo, quod nemo, laborum aspernatur? Eos atque dolores dolor? Asperiores, alias corporis maiores ullam quia quis iure sit. Sed beatae officiis mollitia, est quia libero exercitationem eos doloribus quod atque labore odio voluptatum unde consequuntur in! Dolores, ipsam ratione officiis ducimus architecto alias doloribus quam iste ut quia at.",
+    where:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1881.6287084206322!2d-103.70081010702229!3d19.40128070949323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x84255d1d3cb8c1c7%3A0x93a4f240db0d7169!2sVolc%C3%A1n%20%26%20Caf%C3%A9%20-%20Villas%20-%20Hospedaje!5e0!3m2!1ses!2smx!4v1691095635984!5m2!1ses!2smx",
     promocion: "Preguntar por precios al ser menos de 4 personas",
     amenidades: ["Cocina equipada", "TV", "Wifi", "Estacionamiento"],
     reglas: [""],
@@ -64,6 +71,10 @@ export const villas = [
     fechasE: "Navidad y Año Nuevo",
     fechasEminimoNoches: 2,
     fechasEPrecio: "3,850",
+    descripcion:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut accusamus, adipisci quisquam quis impedit et magnam accusantium reiciendis explicabo, quod nemo, laborum aspernatur? Eos atque dolores dolor? Asperiores, alias corporis maiores ullam quia quis iure sit. Sed beatae officiis mollitia, est quia libero exercitationem eos doloribus quod atque labore odio voluptatum unde consequuntur in! Dolores, ipsam ratione officiis ducimus architecto alias doloribus quam iste ut quia at.",
+    where:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1881.6287084206322!2d-103.70081010702229!3d19.40128070949323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x84255d1d3cb8c1c7%3A0x93a4f240db0d7169!2sVolc%C3%A1n%20%26%20Caf%C3%A9%20-%20Villas%20-%20Hospedaje!5e0!3m2!1ses!2smx!4v1691095635984!5m2!1ses!2smx",
     promocion: "Preguntar por precios al ser menos de 4 personas",
     amenidades: ["Cocina equipada", "TV", "Wifi", "Estacionamiento"],
     reglas: [""],
